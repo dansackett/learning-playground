7dd9883f4d3c42a88f08a5b4004029c9aeff1e6a
Dan Sackett <danesackett@gmail.com>
2014-11-25 22:18:58 -0500

    Problem 12 in the works

--- project-euler/utils.py
+++ project-euler/utils.py
@@ -1,16 +1,27 @@
 """
 Utility Functions
 """
+import math
+
+
 def is_prime(number):
-    """Check if a number if prime"""
+    """
+    Check if a number if prime
+    """
     if number == 2:
         return True
 
     if not number % 2:
         return False
 
-    for x in xrange(3, int(number**.5) + 1, 2):
+    for x in xrange(3, int(math.sqrt(number)) + 1, 2):
         if not number % x:
             return False
 
     return True
+
+
+def get_factors(number):
+    factor_pairs = ([x, number//x] for x in xrange(1, int(math.sqrt(number)) + 1)
+                    if not number % x)
+    return set(sum(factor_pairs, []))
